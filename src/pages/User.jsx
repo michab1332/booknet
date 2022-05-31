d36ba565d570d4a3440915843131d0f36880d589
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2022-05-31 20:03:02 +0200

    add user stats section

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import '../assets/styles/User.css'
+
 export default function User() {
     const { currentUser } = useSelector(state => state.userAuth)
     console.log(currentUser)
     return (
-        <div>User</div>
+        <div className="user">
+            <p className="user__name">Michab</p>
+            <div className="user__stats">
+                <div className="user__stats__item">
+                    <p className="user__stats__item__number">0</p>
+                    <p className="user__stats__item__text">Dodane książki</p>
+                </div>
+                <div className="user__stats__item">
+                    <p className="user__stats__item__number">2</p>
+                    <p className="user__stats__item__text">Przeczytane książki</p>
+                </div>
+                <div className="user__stats__item">
+                    <p className="user__stats__item__number">10</p>
+                    <p className="user__stats__item__text">Polubione książki</p>
+                </div>
+                <div className="user__stats__item">
+                    <p className="user__stats__item__number">3212</p>
+                    <p className="user__stats__item__text">Przeczytane strony</p>
+                </div>
+            </div>
+        </div>
     )
 }
